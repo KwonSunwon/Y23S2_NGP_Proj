a4a6cdc4ada89f88ab5aac06d0de235ad110eb6b
HyunSeok Seong <tjdgustjr112@gmail.com>
2023-11-04 23:51:49 +0900

    Test: 난이도, Packet, ClientInfo
    난이도 enum class, 임시로 Packet과 ClientInfo type.h에 작성

--- Game/Server/Types.h
+++ Game/Server/Types.h
@@ -9,3 +9,20 @@ using uint8 = unsigned __int8;
 using uint16 = unsigned __int16;
 using uint32 = unsigned __int32;
 using uint64 = unsigned __int64;
+
+enum class GAME_LEVEL : byte {
+	EASY,
+	NORMAL,
+	HARD
+};
+
+struct Packet {
+	float x, y;
+	BYTE stateMask;
+};
+
+struct ClientInfo {
+	shared_ptr<queue<Packet>> packetQueptr;
+	GAME_LEVEL level;
+	SOCKET sock;
+};
