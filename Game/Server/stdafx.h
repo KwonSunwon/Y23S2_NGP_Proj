b97aa28a6babe4a27ad841ba7e7ce5f994345237
KwonSunwon <kso00011147@gmail.com>
2023-11-05 16:55:44 +0900

    feat: socket error 처리 함수 추가 - 기존 과제에서 사용하던 ErrQuit, ErrDisplay 함수를 재활용

--- Game/Server/stdafx.h
+++ Game/Server/stdafx.h
@@ -16,3 +16,39 @@ using namespace std;
 
 #include "LockQueue.h"
 #include "Types.h"
+
+// error msg
+void ErrorQuit(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+void ErrorDisplay(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+void ErrorDisplay(int errcode)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, errcode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[?¤ë¥˜] %s\n", (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
