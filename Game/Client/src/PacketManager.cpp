a0e66e4836f03c027cb2dfb0b377b8ced08ce945
Eomjangheon <mpmpm0610@gmail.com>
2023-11-05 20:42:31 +0900

    Feat: PacketManager
    패킷 매니저 클래스 구현
    아직은 연결 부분인 Init만 구현
    나머지 부분은 서버쪽 구현후 테스트 예정

---
+++ Game/Client/src/PacketManager.cpp
@@ -0,0 +1,115 @@
+#pragma once
+#include <memory>
+#include "PacketManager.h"
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <tchar.h> // _T(), ...
+#include <stdio.h> // printf(), ...
+#include <stdlib.h> // exit(), ...
+#include <string.h> // strncpy(), ...
+
+
+PacketManager::PacketManager(GAME_LEVEL level)
+{
+	Initialize(level);
+}
+
+PacketManager::~PacketManager()
+{
+	// 소켓 닫기
+	closesocket(m_sock);
+
+	// 윈속 종료
+	WSACleanup();
+}
+
+void err_quit(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 소켓 함수 오류 출력
+void err_display(int errcode)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, errcode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[오류] %s\n", (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+void PacketManager::Initialize(GAME_LEVEL level)
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		err_quit("Client Socket Init Error");
+	}
+
+	// 소켓 생성
+	m_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (m_sock == INVALID_SOCKET) {
+		err_quit("socket()");
+	}
+
+	// connect()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = connect(m_sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) {
+		err_quit("connect()");
+	}
+
+	//최초 난이도 제공용 send
+	retval = send(m_sock, (char*)&level, sizeof(GAME_LEVEL), 0);
+	if (retval == SOCKET_ERROR) {
+		err_display("send()");
+	}
+}
+
+void PacketManager::SendPacket(Packet)
+{
+	//키 입력시 이 함수를 호출해서 서버로 send할 예정
+}
+
+bool PacketManager::RecvPacket()
+{
+	//toClientEventque가 비어있지 않다면 모두 꺼내서 처리할 예정
+	return false;
+}
+
+shared_ptr<queue<Packet>> PacketManager::GetPacketQueue()
+{
+	return m_toClientEventQue;
+}
+
