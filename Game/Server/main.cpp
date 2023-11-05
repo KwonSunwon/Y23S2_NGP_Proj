fcde6088e0e601c1f7252a4fbec7331f51e0c6a7
KwonSunwon <kso00011147@gmail.com>
2023-11-05 18:17:31 +0900

    feat: 대기 소켓 생성 및 client accept loop 구현 - 테스트용 client-server thread는 임시로 main.cpp 에 제작

--- Game/Server/main.cpp
+++ Game/Server/main.cpp
@@ -26,6 +26,24 @@
 //	}
 //}
 
+// Test를 위한 client-server thraed
+void ClientServerThread(SOCKET client)
+{
+	struct sockaddr_in clientaddr;
+	char addr[INET_ADDRSTRLEN];
+	int addrlen;
+	char buf[24];
+
+	// 클라이언트 정보 얻기
+	addrlen = sizeof(clientaddr);
+	getpeername(client, (struct sockaddr*)&clientaddr, &addrlen);
+	inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+	printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+		addr, ntohs(clientaddr.sin_port));
+
+	closesocket(client);
+}
+
 void print2Digit(BYTE num)
 {
 	for (int i = 7; i >= 0; --i) { //8자리 숫자까지 나타냄
@@ -37,14 +55,43 @@ void print2Digit(BYTE num)
 
 int main()
 {
-	//thread t1(Push);
-	//thread t2(Pop);
-	//thread t3(Pop);
+	//---------------------------------------
+	// wait sock 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		exit(0);
+	SOCKET listenSock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSock == INVALID_SOCKET)
+		exit(0);
+	struct sockaddr_in serverAddr;
+	ZeroMemory(&serverAddr, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(9000);
+	if (bind(listenSock, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		exit(0);
+	if (listen(listenSock, SOMAXCONN) == SOCKET_ERROR)
+		exit(0);
+	//---------------------------------------
+	// client accept loop
+	SOCKET clientSock;
+	struct sockaddr_in clientAddr;
+	int addrLen;
+	thread clientServerThreadHandle;
 
-	//t1.join();
-	//t2.join();
-	//t3.join();
+	printf("Server start, waiting client...\n");
+	while (true) {
+		addrLen = sizeof(clientAddr);
+		clientSock = accept(listenSock, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSock == INVALID_SOCKET) {
+			continue;
+		}
 
+		clientServerThreadHandle = thread(/*Client-Server Thread*/ClientServerThread, clientSock);
+		// detach()를 호출해서 thread를 떼어내야 하나의 handle로 여러 thread를 생성가능
+		clientServerThreadHandle.detach();
+	}
+	// ---------------------------------------
 	// 인게임thread초기화 test
 	initClientInfoQueue();
 	BYTE level;
@@ -52,9 +99,12 @@ int main()
 	eventQueuePtr = InitializeInGameThread(&level);
 	printf("level = %d\n", level);
 	Packet pack;
-	for (int i = 0; i < 3; ++i){
+	for (int i = 0; i < 3; ++i) {
 		eventQueuePtr[i]->TryPop(pack);
 		printf("%.1f, %.1f \n", pack.x, pack.y);
 		print2Digit(pack.stateMask);
 	}
+
+	closesocket(listenSock);
+	WSACleanup();
 }
