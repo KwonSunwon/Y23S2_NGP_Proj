d8880a7197705774b86b65391e55dd055b7b1b43
KwonSunwon <kso00011147@gmail.com>
2023-11-10 13:08:04 +0900

    Feat: InGame thread를 호출하기 전에 ClientInfoQueue에서 WaitPop을 통해 꺼내와 전달해주는 것으로 변경 - 이전에 InGameThread에서 Init이 끝나기 전에 Lobby에서 인원을 잘 못 파악해 InGameThread를 두 번 호출하는 경우가 발생함

--- Game/Server/LobbyThread.cpp
+++ Game/Server/LobbyThread.cpp
@@ -12,7 +12,15 @@ void LobbyThread()
 		for (int level = 0; level < NUM_OF_LEVEL; level++) {
 			if (ClientInfoQueue[level].Size() == NUM_OF_PLAYER) {
 				// Game Start
-				thread th = thread(/*InGameThread*/InGameThread, static_cast<GAME_LEVEL>(level));
+				array<EventQueues, NUM_OF_PLAYER> eventQueues;
+				ClientInfo tempClientInfo;
+				for (auto& player : eventQueues) {
+					ClientInfoQueue[level].WaitPop(tempClientInfo);
+					player.toClientEventQueue = tempClientInfo.toClientEventQueue;
+					player.toServerEventQueue = tempClientInfo.toServerEventQueue;
+				}
+
+				thread th = thread(/*InGameThread*/InGameThread, static_cast<GAME_LEVEL>(level), eventQueues);
 				th.detach();
 #ifdef _DEBUG_LOOBY
 				cout << "Game Start" << endl;
