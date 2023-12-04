55c39a4c3e3a812f7a4cc314deeae0a18dc75d80
HyunSeok Seong <tjdgustjr112@gmail.com>
2023-12-04 19:54:39 +0900

    Fix: 선형 보간 처리

--- Game/Client/src/easyStage.cpp
+++ Game/Client/src/easyStage.cpp
@@ -146,12 +146,10 @@ void EasyStage::update()
 				if (p->getPlayerNum() == playerNum) {
 					//cout << accX << " " << accY << endl;
 					glm::vec3 temp = p->getPos();
-					if ((temp.x - accX) * (temp.x - accX) + (temp.y - accY) * (temp.y - accY) > 0.03 * 0.03){
-						float newX = temp.x + (accX - temp.x) * 0.5;
-						float newY = temp.y + (accY - temp.y) * 0.5;
-						p->setPos(glm::vec3(newX, newY, 0.7));
-					}
-	
+					float newX = temp.x + (accX - temp.x) * 0.5;
+					float newY = temp.y + (accY - temp.y) * 0.5;
+					p->setPos(glm::vec3(newX, newY, 0.7));
+
 				}
 			}
 		}
